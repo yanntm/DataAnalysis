716442964f4ac0b11dc3ecdb77196874d6bc677b
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-02-14 20:33:29 +0100

    début parsing du json avec l'outil jq

--- logs.sh
+++ logs.sh
@@ -2,13 +2,20 @@
 
 # recuperation de l'ID du Build
 
-nb = $2 - $1
-build=$(curl -s "https://api.travis-ci.org/v3/repo/yanntm%2FITS-Tools-pnmcc/builds?sort_by=id&limit=$nb&offset=$1"| jq -r .builds[].id)
+let nb=$2-$1
+echo $nb
+json=`curl "https://api.travis-ci.org/v3/repo/yanntm%2FITS-Tools-pnmcc/builds?sort_by=id&limit=2&offset=530"`
+builds_id=`echo $json | jq '. | .builds[].id'`
 
-echo $build
-nb_job = $1+15
-for job_id in `seq $1 $nb_job`;
+for build_id in "${builds_id[@]}"
 do 
-	echo "Build $i"
-	wget https://api.travis-ci.org/v3/job/job_id/log.txt
+	echo "$build_id"
 done
+
+#echo $json
+#nb_job=$1+15
+#for job_id in `seq $1 $nb_job`;
+#do 
+#	echo "Build $i"
+#	wget https://api.travis-ci.org/v3/job/job_id/log.txt
+#done
--- travis.pl
+++
@@ -1,1 +0,0 @@
-curl "https://api.travis-ci.org/v3/repo/yanntm%2FITS-Tools-pnmcc/builds?sort_by=id&limit=2&offset=530";
