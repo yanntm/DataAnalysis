cabb38602e662aa31a09c9b3c04a4c40e886cf1c
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-02-15 02:34:25 +0100

    logs.sh jusqu'à l'étape d'écriture du log de job travis dans un .txt

--- logs.sh
+++ logs.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# recuperation de l'ID du Build
+cd ./travisLogs
+ls
 
-let nb=$2-$1
-echo $nb
-json=`curl "https://api.travis-ci.org/v3/repo/yanntm%2FITS-Tools-pnmcc/builds?sort_by=id&limit=2&offset=530"`
-builds_id=`echo $json | jq '. | .builds[].id'`
+let length=$2-$1+1
+let skipped=$1-1
+echo "length : $length, skip until : $skipped"
 
-for build_id in "${builds_id[@]}"
-do 
-	echo "$build_id"
+json=$(curl "https://api.travis-ci.org/v3/repo/yanntm%2FITS-Tools-pnmcc/builds?sort_by=id&limit=$length&offset=$skipped")
+
+for ((i=0; i<length; i++))
+do
+	#build_num=$(echo $json | jq -r --arg i $i '. | .builds[$i|tonumber].number')
+	
+	jobs_ids=( $(echo $json | jq -r --arg i $i '. | .builds[$i|tonumber].jobs[].id') )
+	
+	#printf "build number : $build_num\n" 
+	#echo ${jobs_ids[*]}
+
+	for job_id in ${jobs_ids[@]}
+	do
+		echo $job_id
+		curl "https://api.travis-ci.org/v3/job/$job_id/log.txt" > log$job_id.txt
+	done
 done
 
-#echo $json
-#nb_job=$1+15
-#for job_id in `seq $1 $nb_job`;
-#do 
-#	echo "Build $i"
+
 #	wget https://api.travis-ci.org/v3/job/job_id/log.txt
-#done
+
