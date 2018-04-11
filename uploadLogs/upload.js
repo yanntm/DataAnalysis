997057030ffb0576ba509400584c2aa91f276ddb
Vincent Ha Vinh <vincent.havinh@hotmail.fr>
2018-04-11 19:42:34 +0200

    début de l'utilisation de l'API google Drive.
    chercher File API pour html, pour input un fichier.

--- uploadLogs/upload.js
+++ uploadLogs/upload.js
@@ -1,13 +1,18 @@
 //var indexId = "1dKd2VkHRqKccKgaT_l4LtvH4zoiDhWfo-UuHBEF4Mko";
-
+console.log("a");
 // Client ID and API key from the Developer Console
 var CLIENT_ID = '1034390180382-m8182r44nbneu9ubqrhh5n2k1udcllpi.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyAIQTtIoHf8_gX1gLZhZQACOAShvPNaW7c';
+console.log("b");
 // Array of API discovery doc URLs for APIs used by the quickstart
 var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+DISCOVERY_DOCS.push("https://www.googleapis.com/discovery/v1/apis/drive/v3/rest");
+console.log("c");
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
 var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+SCOPES += " https://www.googleapis.com/auth/drive.file";
+console.log("d");
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
 
@@ -16,7 +21,9 @@ var signoutButton = document.getElementById('signout-button');
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
+	console.log("e");
 	gapi.load('client:auth2', initClient);
+	console.log("f");
 }
 
 /**
@@ -24,18 +31,21 @@ function handleClientLoad() {
  *  listeners.
  */
  function initClient() {
+ 	console.log("g");
 	gapi.client.init({
 		apiKey: API_KEY,
 		clientId: CLIENT_ID,
 		discoveryDocs: DISCOVERY_DOCS,
 		scope: SCOPES
 	}).then(function () {
+		console.log("h");
 		// Listen for sign-in state changes.
 		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 		// Handle the initial sign-in state.
 		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
 		authorizeButton.onclick = handleAuthClick;
 		signoutButton.onclick = handleSignoutClick;
+		console.log("i");		
 	});
  }
 
@@ -47,7 +57,7 @@ function handleClientLoad() {
  	if (isSignedIn) {
  		authorizeButton.style.display = 'none';
  		signoutButton.style.display = 'block';
- 		listMajors();
+ 		exec();
  	} else {
  		authorizeButton.style.display = 'block';
  		signoutButton.style.display = 'none';
@@ -81,21 +91,22 @@ function appendPre(message) {
 }
 
 /**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ *
  */
- function listMajors() {
+ function exec() {
+
+ 	/*LIRE UNE SHEET AVEC L'API GOOGLE SHEET*/
  	gapi.client.sheets.spreadsheets.values.get({
- 		spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
- 		range: 'Class Data!A2:E',
+ 		spreadsheetId: '14cQyAjuJiXlpnLGenrAB4RPeRFZA99eVDGcW05HrXio',
+ 		range: 'C2:E5',
  	}).then(function(response) {
  		var range = response.result;
  		if (range.values.length > 0) {
- 			appendPre('Name, Major:');
  			for (i = 0; i < range.values.length; i++) {
  				var row = range.values[i];
 				// Print columns A and E, which correspond to indices 0 and 4.
-				appendPre(row[0] + ', ' + row[4]);
+				appendPre(row[0]+', '+row[1]+', '+row[2]+', '+
+					row[3]+', '+row[4]+', '+row[5]+', '+row[6	]) ;
 			}
 		} else {
 			appendPre('No data found.');
@@ -103,4 +114,25 @@ function appendPre(message) {
 	}, function(response) {
 		appendPre('Error: ' + response.result.error.message);
 	});
+
+ 	/*UPLOAD UN FICHIER AVEC L'API GOOGLE DRIVE*/
+ 	var fileMetadata = {'name': 'photo.jpg'};
+ 	var media = {
+ 		mimeType: 'image/jpeg',
+ 		body: fs.createReadStream('files/photo.jpg')
+ 	};
+
+ 	drive.files.create({
+ 		resource: fileMetadata,
+ 		media: media,
+ 		fields: 'id'
+ 	}, function (err, file) {
+ 		if (err) {
+			// Handle error
+			console.error(err);
+		} else {
+			console.log('File Id: ', file.id);
+		}
+	});
+
  }
